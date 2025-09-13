bbff97b8489d6af9e1cbbc0c3b6a06f95a497e86
Surya <suryatk2007@gmail.com>
2025-09-13 12:47:40 +0530

    feat(server): add basic Axum hello world endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+#[tokio::main]
+async fn main(){
+    let app=Router::new().route("/", get(root));
+    let addr:SocketAddr="127.0.0.1:3000".parse().unwrap();
+    println!("Server running in http://{addr}");
+    axum::serve(tokio::net::TcpListener::bind(&addr).await.unwrap(),app).await.unwrap();
+}
+async fn root()->&'static str{
+    "Hello"
 }
